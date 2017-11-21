372a3f1ce02c546df95026328304380ed4a4f0e3
sloba88 <slobodan.stanic@futurice.com>
2017-11-21 20:42:19 +0200

    add support for parent

--- lib/logstash/outputs/amazon_es.rb
+++ lib/logstash/outputs/amazon_es.rb
@@ -99,6 +99,10 @@ class LogStash::Outputs::AmazonES < LogStash::Outputs::Base
   # This can be dynamic using the `%{foo}` syntax.
   config :routing, :validate => :string
 
+  # For child documents, ID of the associated parent.
+  # This can be dynamic using the %{foo} syntax.
+  config :parent, :validate => :string
+
 
 # Set the endpoint of your Amazon Elasticsearch domain. This will always be array of size 1
 #     ["foo.us-east-1.es.amazonaws.com"]
@@ -304,6 +308,7 @@ class LogStash::Outputs::AmazonES < LogStash::Outputs::Base
       :_index => event.sprintf(@index),
       :_type => type,
       :_routing => @routing ? event.sprintf(@routing) : nil
+      :parent => @parent ? event.sprintf(@parent) : nil
     }
 
     params[:_upsert] = LogStash::Json.load(event.sprintf(@upsert)) if @action == 'update' && @upsert != ""
